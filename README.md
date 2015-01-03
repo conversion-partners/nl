4b28190df6de36dc892f89eb1960e17e066cb672
jessec <robertjessecooke@gmail.com>
2015-01-03 22:00:56 +0100

    cc

--- README.md
+++
@@ -1,4 +0,0 @@
-nl
-==
-
-Dutch pages
---
+++ js/index.js
@@ -0,0 +1,8 @@
+$('document').ready(function() {
+	var options = { videoId: 'RtS1c5XmjoY', start: 3 };
+	// backup if video goes offline
+	//var options = { videoId: 'jsBfId4NsN4', start: 3 , ratio: 4/3 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
